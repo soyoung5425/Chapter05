49445109cc279054742c3fe9c8ee6fc06b6f1ab4
Yun Soyoung <soyoung@naver.com>
2023-03-20 13:45:16 +0900

    2303201345

--- YunSoYoung_ex3/Program.cs
+++ YunSoYoung_ex3/Program.cs
@@ -6,22 +6,65 @@ namespace YunSoYoung_ex3
     {
         static void Main(string[] args)
         {
-            int a = 5;
-
-            while (a>=0)
+            int i = 0;
+            while (i<5)
             {
-                int i = 0;
-                while (a >= i)
+                int j = 0;
+                while(j<=i)
                 {
-                    if(i<5)
-                    {
-                        break;
-                    }
                     Console.Write("*");
-                    i -= 1;
+                    j++;
                 }
+                Console.WriteLine();
+                i++;
             }
+
             Console.WriteLine();
+
+            int a = 0;
+            while(a<5)
+            {
+                int b = 5;
+                while(b>a)
+                {
+                    Console.Write("*");
+                    b--;
+                }
+                Console.WriteLine();
+                a++;
+            }
+
+            Console.WriteLine();
+
+            int c = 0;
+            do
+            {
+                int d = 0;
+                while (d <= c)
+                {
+                    Console.Write("*");
+                    d++;
+                }
+                Console.WriteLine();
+                c++;
+            }
+            while (c < 5);
+
+            Console.WriteLine();
+
+            int e = 0;
+            do
+            {
+                int f = 5;
+                while (f > e)
+                {
+                    Console.Write("*");
+                    f--;
+                }
+                Console.WriteLine();
+                e++;
+            }
+            while (e < 5);
         }
     }
 }
--- YunSoYoung_ex4/Program.cs
+++ YunSoYoung_ex4/Program.cs
@@ -5,6 +5,25 @@ namespace YunSoYoung_ex4
     class Program
     {
         static void Main(string[] args)
-        {}
+        {
+            Console.Write("반복 횟수를 입력하세요 :");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if(number<=0)
+            {
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
